6fb7aa94525ad0fb44f49c41befb1f8a4fadd95b
VEW1102 <VEW1102@ad.uniandes.edu.co>
2022-05-09 14:58:58 -0500

    caso simetrico con archivos hecho

--- CifradoSimetrico/src/Main3.java
+++ CifradoSimetrico/src/Main3.java
@@ -1,5 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.security.NoSuchAlgorithmException;
@@ -7,6 +10,7 @@ import java.util.Scanner;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.security.auth.callback.TextOutputCallback;
 
 /**
  * 
@@ -40,29 +44,39 @@ public class Main3 {
 
 		System.out.println ("Entrada recibida por teclado es: \"" + entradaTeclado +"\"");
 
+
 		byte[] arregloBytes =entradaTeclado.getBytes();
 
 		imprimir(arregloBytes);
 		System.out.println("//////////////////////////////////////////////////////////");
 		KeyGenerator keygen = KeyGenerator.getInstance(ALGORITMO);
 		SecretKey secretKey = keygen.generateKey();
-		
+
 		//Genere una llave secreta y guárdela en un archivo
-		FileOutputStream archivo = new FileOutputStream("nombreArchivo");
+		FileOutputStream archivo = new FileOutputStream("./data/ejemplo.txt");
 		ObjectOutputStream oos = new ObjectOutputStream(archivo);
-		
+
 		oos.writeObject(secretKey);
 		oos.close();
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		byte[] arregloBytesCifrado= Simetrico.cifrar(secretKey, entradaTeclado);
 		imprimir(arregloBytesCifrado);
+
+		//Cifre un mensaje de entrada. Almacene el texto cifrado en un archivo.
+		archivo = new FileOutputStream("./data/ejemplo.txt");
+		oos =  new ObjectOutputStream(archivo);
+
+		oos.writeObject(new String (arregloBytesCifrado));
+		oos.close();
+
 		System.out.println("//////////////////////////////////////////////////////////");
+		
+		
 		byte[] arregloBytesDeCifrado=Simetrico.descifrar(secretKey, arregloBytesCifrado);
 		imprimir(arregloBytesDeCifrado);
 
@@ -77,5 +91,27 @@ public class Main3 {
 		}
 		System.out.println(contenido[i]+ " ");
 	}
+
+	public static File crearArchivo(String contenidoArchivo)
+	{
+		try {
+			String ruta = "/ruta/archivo.txt";
+			String contenido = contenidoArchivo;
+			File file = new File(ruta);
+			// Si el archivo no existe es creado
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(contenido);
+			bw.close();
+			return file;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 
 }

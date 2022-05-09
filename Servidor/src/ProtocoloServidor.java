c3a2d069b9d1f2f1cb95f8faf0836486bf148074
andres <af.garciab@uniandes.edu.co>
2022-05-09 00:53:05 -0500

    casi la embarro

--- Servidor/src/ProtocoloServidor.java
+++ Servidor/src/ProtocoloServidor.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * 
  */
@@ -7,5 +11,26 @@
  *
  */
 public class ProtocoloServidor {
+
+	public static void procesar(BufferedReader pIn, PrintWriter pOut) throws IOException{
+		// TODO Auto-generated method stub
+		String inputLine;
+		String outputLine;
+
+		//lee del flujo de entrada
+		inputLine= pIn.readLine();
+		System.out.println("Entrada a procesar: " + inputLine);
+
+		//procesa la entrada
+		outputLine=inputLine;
+		if(inputLine.equals("ok"))
+		{
+			outputLine="adios";
+		}
+
+		//escribe en el flujo de salida
+		pOut.println(outputLine);
+		System.out.println("salida procesada: "+ outputLine);
+	}
 
 }
